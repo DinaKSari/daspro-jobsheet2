993cfec8ebaf1806a7da61b223108ae4e2057c0e
DinaKSari <dinakumala7070@gmail.com>
2024-09-06 17:00:39 +0700

    Menambahkan file MyFirstJava.java via upload

---
+++ MyFirstJava11.java
@@ -0,0 +1,16 @@
+public class MyFirstJava11 {
+
+    public static void main (String[] args) {
+        System.out.print("\n------------------------");
+        System.out.print("\n--Data Mahasiswa Polinema--");
+        System.out.print("\n------------------------");
+        System.out.print("\nNAMA: DINA KUMALA SARI");
+        System.out.print("\nNIM: 244107020072");
+        System.out.print("\nKELAS: 1A");
+        System.out.print("\nNO ABSEN: 11");
+        System.out.print("\nProdi: Teknik Informatika");
+        System.out.print("\nAlamat: Jl.Mondoroko 1 no.30");
+        System.out.print("\nNo HP: 082372010600");
+        System.out.print("\nHobi: Menyanyi");
+    }
+}
